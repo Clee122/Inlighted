0d8414b081f771ceda0bc28139cbb0532ec0560b
DraSoll <k.onyxwafer@gmail.com>
2026-07-31 23:19:53 +1000

    SignFunctionality

---
+++ Assets/Scripts/ToolTipAppear.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ToolTipAppear : MonoBehaviour
+{
+
+   
+    public GameObject ChosenTip;
+    public Vector3 location;
+    private GameObject ToolTip;
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SpawnToolTip();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        KillToolTip();
+
+    }
+
+    void SpawnToolTip()
+    {
+        ToolTip = Instantiate(ChosenTip,location,transform.rotation);
+
+    }
+
+    void KillToolTip()
+    {
+        Destroy(ToolTip);
+    }
+
+}
+
